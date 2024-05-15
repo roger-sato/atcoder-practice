1579e7f03955f694278a10809c362f88017cc3fe
roger-sato <pyomta13@gmail.com>
2024-05-16 06:39:21 +0900

    solve abc353/c

--- abc353/src/bin/c.rs
+++ abc353/src/bin/c.rs
@@ -10,6 +10,22 @@ const MOD: usize = 1000000007;
 fn main() {
     input! {
         n:usize,
+        mut a:[usize;n]
     }
-    println!("{}", n);
+    let mut count = 0;
+    a.sort();
+
+    let mut right = n;
+    for i in 0..n {
+        right = cmp::max(right, i + 1);
+        while right - 1 > i && a[right - 1] + a[i] >= 100000000 {
+            right -= 1;
+        }
+        count += n - right
+    }
+
+    println!(
+        "{}",
+        ((n - 1) * a.iter().sum::<usize>()) - 100000000 * count
+    );
 }
