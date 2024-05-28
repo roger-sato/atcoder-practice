62eccae57c745738ff3f2679cdb8bb2147c0f401
roger-sato <pyomta13@gmail.com>
2024-05-28 17:08:15 +0900

    solve abc253/d

--- abc253/src/bin/d.rs
+++ abc253/src/bin/d.rs
@@ -10,6 +10,22 @@ const MOD: usize = 1000000007;
 fn main() {
     input! {
         n:usize,
+        a:usize,
+        b:usize
     }
-    println!("{}", n);
+    let ans = sum_up(n, 1) - sum_up(n, a) - sum_up(n, b) + sum_up(n, a * b / gcd(a, b));
+
+    println!("{}", ans);
+}
+
+fn sum_up(n: usize, d: usize) -> usize {
+    let nd: usize = (n as f64 / d as f64).floor() as usize;
+    nd * (d + nd * d) / 2
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a;
+    }
+    return gcd(b, a % b);
 }
